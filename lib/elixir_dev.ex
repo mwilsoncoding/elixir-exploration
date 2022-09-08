14bd06fce2d870933fd81cc1861510f6f77f6389
Max Wilson <io@maxwilson.dev>
2022-09-08 13:28:54 -0400

    create elixir project with dialyzer and credo

---
+++ lib/elixir_dev.ex
@@ -0,0 +1,21 @@
+defmodule ElixirDev do
+  @moduledoc """
+  Documentation for `ElixirDev`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirDev.hello()
+      :world
+
+  """
+  def hello do
+    "world"
+    |> IO.puts()
+
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule ElixirDev.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_dev,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      description: "Example Elixir Dev Environment",
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp package() do
+    %{
+      licenses: ["Apache-2.0"],
+      maintainers: ["Max Wilson"],
+      links: %{}
+    }
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
